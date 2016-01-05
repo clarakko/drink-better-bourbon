dd30f4a837d56386665ba9674833238dccc7d19f
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-05 11:11:10 -0500

    remove extraneous route comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     root to: 'static_pages#index'
-    # delete 'sign_out', to: 'devise/sessions#destroy',
-    # as: :destroy_user_session
   end
 end
