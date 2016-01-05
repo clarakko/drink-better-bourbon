b62a2f4d45bc766dd599d93f2a56aac7c483943b
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-05 10:43:32 -0500

    fix more hounds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise(
+    :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable)
 
   validates :username, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users
   devise_scope :user do
     root to: 'static_pages#index'
     # delete 'sign_out', to: 'devise/sessions#destroy',
