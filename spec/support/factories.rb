42dc6ddb8ab421013441e397eefa026dc7a21f16
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-05 13:53:35 -0500

    passify the hound

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     sequence(:username) { |n| "Pablo#{n}" }
-    sequence(:email) { |n| "pablo#{n}@user.com"}
+    sequence(:email) { |n| "pablo#{n}@user.com" }
     password 'password'
     password_confirmation 'password'
   end
