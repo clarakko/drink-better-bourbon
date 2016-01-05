a7c6b03941c7a9ce795a6fe75ac9cad3e6433352
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-05 17:12:33 -0500

    add user delete

--- spec/features/users/user_updates_spec.rb
+++ spec/features/users/user_updates_spec.rb
@@ -12,26 +12,22 @@ feature 'user updates information', %{
   [x] I must confirm my new password if I wish to change my password
 } do
 
-  scenario "authenticated user goes to settings page" do
-    user = FactoryGirl.create(:user)
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before :each do
     visit root_path
-    expect(page).to_not have_content('Edit User')
     click_link 'Sign In'
     fill_in 'Username', with: user.username
     fill_in 'Password', with: user.password
     click_button 'Sign In'
-    click_on "Settings"
+    click_on 'Settings'
+  end
+
+  scenario "authenticated user goes to settings page" do
     expect(page).to have_content("Edit User")
   end
 
   scenario "User changes one field only" do
-    user = FactoryGirl.create(:user)
-    visit root_path
-    click_link 'Sign In'
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Sign In'
-    click_on "Settings"
     fill_in "Email", with: 'jojo@gmail.com'
     fill_in "Current password", with: user.password
     click_button 'Update'
@@ -39,26 +35,12 @@ feature 'user updates information', %{
   end
 
   scenario 'User fails to enter current password' do
-    user = FactoryGirl.create(:user)
-    visit root_path
-    click_link 'Sign In'
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Sign In'
-    click_on "Settings"
     fill_in "Email", with: 'jojo@gmail.com'
     click_button 'Update'
     expect(page).to have_content('Current password can\'t be blank')
   end
 
   scenario 'User fails to confirm new password' do
-    user = FactoryGirl.create(:user)
-    visit root_path
-    click_link 'Sign In'
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Sign In'
-    click_on "Settings"
     fill_in "Password", with: 'NewPassword'
     fill_in "Current password", with: user.password
     click_button 'Update'
