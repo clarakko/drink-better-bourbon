507b9578c8014e422da8b9b5539ed01e92721599
Brian Crowley <crowlebf@gmail.com>
2016-01-06 11:20:52 -0500

    updates given by mlg

--- app/models/bourbon.rb
+++ app/models/bourbon.rb
@@ -4,4 +4,6 @@ class Bourbon < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :proof, presence: true
+  validates :proof, numericality: { :greater_than => 0, :less_than_or_equal_to => 200 }
+  validates :proof, length: { maximum: 4 }
 end
--- spec/features/visitor_can_see_list_of_bourbons_spec.rb
+++ spec/features/visitor_can_see_list_of_bourbons_spec.rb
@@ -4,12 +4,14 @@ feature "The bourbon index page: " do
   let!(:bourbon) { FactoryGirl.create(:bourbon) }
   scenario "visitor can see list of bourbons on index page" do
     visit bourbons_path
+
     expect(page).to have_content bourbon.name
     expect(page).to have_content bourbon.distillery
   end
 
   scenario "visitor clicks link for bourbon show page" do
     visit bourbons_path
+
     click_link bourbon.name
     expect(current_path).to eq bourbon_path(bourbon)
     expect(page).to have_content bourbon.distillery
