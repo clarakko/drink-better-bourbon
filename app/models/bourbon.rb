a8de4eba5a6d74b0ed561a68067f34eb9c07dc1c
Brian Crowley <crowlebf@gmail.com>
2016-01-06 11:33:29 -0500

    hound

--- app/models/bourbon.rb
+++ app/models/bourbon.rb
@@ -4,6 +4,7 @@ class Bourbon < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :proof, presence: true
-  validates :proof, numericality: { :greater_than => 0, :less_than_or_equal_to => 200 }
+  validates :proof, numericality: { :greater_than => 0,
+    :less_than_or_equal_to => 200 }
   validates :proof, length: { maximum: 4 }
 end
