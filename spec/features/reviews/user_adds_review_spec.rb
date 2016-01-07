6373465c2a8c3d46030e7ade15d60a227fca1cbf
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-07 17:18:41 -0500

    add authorization

--- spec/features/reviews/user_adds_review_spec.rb
+++ spec/features/reviews/user_adds_review_spec.rb
@@ -6,11 +6,12 @@ feature 'user creates review', %{
   I want to add a review
 
   Acceptance Criteria
-  [] I can add a review
-  [] I cannot add a review without filling all fields
+  [x] I am not authenticated and I cannot add a review
+  [x] I am authenticated and I can add a review
+  [x] I cannot add a review without filling all fields
 } do
 
-  let!(:bourbon) { FactoryGirl.create(:bourbon) }
+  let!(:bourbon) { FactoryGirl.create(:bourbon, user: user) }
   let!(:user) { FactoryGirl.create(:user) }
 
   before(:each) do
@@ -24,7 +25,17 @@ feature 'user creates review', %{
     click_link 'New Review'
   end
 
-  scenario "a user adds a review" do
+  scenario "an unauthenticated user cannot add a review" do
+    click_link 'Sign Out'
+    visit bourbon_path(bourbon)
+
+    expect(page).not_to have_link('Add Review')
+
+    visit new_bourbon_review_path(bourbon)
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+
+  scenario "an authenticated user adds a review" do
     review = FactoryGirl.create(:review)
     fill_in 'Description', with: review.description
     fill_in 'Rating', with: review.rating
