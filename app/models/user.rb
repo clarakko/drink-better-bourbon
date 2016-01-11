b7cc14a7f6f74bbb1eb958699b200a5755181d3b
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-11 16:34:17 -0500

    fix deleted user bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :reviews
+  has_many :bourbons
+  has_many :reviews, dependent: :destroy
+
   devise(
     :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable)
