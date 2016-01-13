43a71ec4aa1db64570833370142a0fe8c030c0f4
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-01-13 17:49:46 -0500

    Wooof

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,10 +1,9 @@
 class ReviewMailer < ApplicationMailer
-
   def new_review(review)
     @review = review
      mail(
-      to: review.bourbon.user.email,
-      subject: "Review Added"
-     )
-  end
+     	to: review.bourbon.user.email,
+     	subject: "Review Added"
+     	)
+	end
 end
