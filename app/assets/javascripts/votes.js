9e92676401e5b0b5cf8115f6cf53bb9d3921255f
Brian Crowley <crowlebf@gmail.com>
2016-01-13 14:30:07 -0500

    THE HOUND

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,45 +1,45 @@
-$('.upvote').on('click', function(event) {
+$(".upvote").on("click", function(event) {
   event.preventDefault();
-  var urlID = $(this).attr('href');
-  var delimeter = '/';
+  var urlID = $(this).attr("href");
+  var delimeter = "/";
   var string = urlID;
   var splat = string.split(delimeter);
   var id = splat[2];
 
-  var request = $.ajax({
-    type: 'POST',
+  $.ajax({
+    type: "POST",
     url: "/api/v1/votes/upvote",
-    dataType: 'json',
+    dataType: "json",
     data: {
       review: {
         id: id
       }
     },
     success: function(response) {
-      $('#vote-' + id).html("<li>" + response + "</li>");
+      $("#vote-" + id).html("<li>" + response + "</li>");
     }
   });
   return false;
 });
 
-$('.downvote').on('click', function(event) {
+$(".downvote").on("click", function(event) {
   event.preventDefault();
-  var urlID = $(this).attr('href');
-  var delimeter = '/';
+  var urlID = $(this).attr("href");
+  var delimeter = "/";
   var string = urlID;
   var splat = string.split(delimeter);
   var id = splat[2];
   $.ajax({
-    type: 'POST',
+    type: "POST",
     url: "/api/v1/votes/downvote",
-    dataType: 'json',
+    dataType: "json",
     data: {
       review: {
         id: id
       }
     },
     success: function(response) {
-      $('#vote-' + id).html("<li>" + response + "</li>");
+      $("#vote-" + id).html("<li>" + response + "</li>");
     }
   });
   return false;
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,72 +1,36 @@
-# class VotesController < ApplicationController
-#   before_action :authenticate_user!
-#   before_action :pre_vote
-#
-#   def show
-#     redirect_to bourbon_path(@review.bourbon)
-#   end
-#
-#   def upvote
-#     @value.vote == 1 ? @value.vote -= 1 : @value.vote = 1
-#     respond_to do |format|
-#       @value.save
-#       @total_votes = Vote.group(:review_id).sum(:vote)
-#       format.json { render json: @total_votes[@review.id] }
-#     end
-#   end
-#
-#   def downvote
-#     @value.vote == -1 ? @value.vote += 1 : @value.vote = -1
-#     respond_to do |format|
-#       @value.save
-#       @total_votes = Vote.group(:review_id).sum(:vote)
-#       format.json { render json: @total_votes[@review.id] }
-#     end
-#   end
-#
-#   protected
-#
-#   def pre_vote
-#     @review = Review.find(params[:review_id])
-#     @value = Vote.find_or_initialize_by(review: @review, user: current_user)
-#   end
-# end
-
-
 class VotesController < ApplicationController
   before_action :before
 
-
-    def upvote
-      if @value.vote == 0 || @value.vote == -1
-        @value.vote = 1
-        @value.save
-        flash[:notice] = "Upvoted!"
-      elsif @value.vote == 1
-        @value.vote -= 1
-        @value.save
-        flash[:notice] = "Upvote cancelled!"
-      end
-      redirect_to bourbon_path(@review.bourbon)
+  def upvote
+    if @value.vote == 0 || @value.vote == -1
+      @value.vote = 1
+      @value.save
+      flash[:notice] = "Upvoted!"
+    elsif @value.vote == 1
+      @value.vote -= 1
+      @value.save
+      flash[:notice] = "Upvote cancelled!"
     end
-
-    def downvote
-      if @value.vote == 0 || @value.vote == 1
-        @value.vote = -1
-        @value.save
-        flash[:notice] = "Downvoted!"
-      elsif @value.vote == -1
-        @value.vote += 1
-        @value.save
-        flash[:notice] = "Downvote cancelled!"
-      end
-      redirect_to bourbon_path(@review.bourbon)
-    end
-
-    protected
-
-    def before
-      @review = Review.find(params[:review_id])
-      @value = Vote.find_or_initialize_by(review: @review, user: current_user)
-    end
+    redirect_to bourbon_path(@review.bourbon)
   end
+
+  def downvote
+    if @value.vote == 0 || @value.vote == 1
+      @value.vote = -1
+      @value.save
+      flash[:notice] = "Downvoted!"
+    elsif @value.vote == -1
+      @value.vote += 1
+      @value.save
+      flash[:notice] = "Downvote cancelled!"
+    end
+    redirect_to bourbon_path(@review.bourbon)
+  end
+
+  protected
+
+  def before
+    @review = Review.find(params[:review_id])
+    @value = Vote.find_or_initialize_by(review: @review, user: current_user)
+  end
+end
