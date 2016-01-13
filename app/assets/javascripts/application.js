de8dc661e750184aa7ab16d1f2a24f4a57470a85
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-13 13:44:36 -0500

    fix nonworking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,48 +1,18 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require_tree .
+
 $(function(){ $(document).foundation(); });
-
-$('.upvote').on('click', function(event) {
-  event.preventDefault();
-  var urlID = $(this).attr('href');
-  var delimeter = '/';
-  var string = urlID;
-  var splat = string.split(delimeter);
-  var id = splat[2];
-
-  var request = $.ajax({
-    type: 'POST',
-    url: "/api/v1/votes/upvote",
-    dataType: 'json',
-    data: {
-      review: {
-        id: id
-      }
-    },
-    success: function(response) {
-      $('#vote-' + id).html("<li>" + response + "</li>");
-    }
-  });
-  return false;
-});
-
-$('.downvote').on('click', function(event) {
-  event.preventDefault();
-  var urlID = $(this).attr('href');
-  var delimeter = '/';
-  var string = urlID;
-  var splat = string.split(delimeter);
-  var id = splat[2];
-  $.ajax({
-    type: 'POST',
-    url: "/api/v1/votes/downvote",
-    dataType: 'json',
-    data: {
-      review: {
-        id: id
-      }
-    },
-    success: function(response) {
-      $('#vote-' + id).html("<li>" + response + "</li>");
-    }
-  });
-  return false;
-});
