5f00033150c307db2c70de9677a8b8171b22656c
Jonathan Devers <jonathan.devers@gmail.com>
2016-01-15 13:29:53 -0500

    basic seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-200.times do |i|
-  Bourbon.create(
-    name: "Jim Beam #{i}",
-    proof: 80,
-    distillery: "Brown-Formen",
-    varietal: "Blended"
-  )
-end
-
-User.create(
+admin = User.create(
   username: 'admin', email: 'admin@admin.com', password: 'password',
   password_confirmation: 'password', role: 'admin')
+
+50.times do |i|
+  Bourbon.create(
+    user_id: admin.id,
+    name: "Elegant Firewater #{i}",
+    proof: 160,
+    distillery: "House of Bourbon"
+  )
+end
