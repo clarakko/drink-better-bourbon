36b2357863edb12bb54e0a91117abb4b22719118
Craig Dickson <craigmichaeldickson@gmail.com>
2016-01-15 14:32:02 -0500

    Pagination styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
+@user = User.create(
+  username: 'admin2', email: 'admin2@admin.com', password: 'password',
+  password_confirmation: 'password', role: 'admin')
+
 200.times do |i|
   Bourbon.create(
     name: "Jim Beam #{i}",
     proof: 80,
     distillery: "Brown-Formen",
-    varietal: "Blended"
+    varietal: "Blended",
+    user_id: @user.id
   )
 end
-
-User.create(
-  username: 'admin', email: 'admin@admin.com', password: 'password',
-  password_confirmation: 'password', role: 'admin')
